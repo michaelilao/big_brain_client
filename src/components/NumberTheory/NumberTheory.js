78c24b8eb80c4f0340a18e725bdfef60ae164b1e
Ardhendu Barge <bargea@mcmaster.ca>
2021-03-30 18:49:10 -0400

    initial commit for num theory mini game

---
+++ src/components/NumberTheory/NumberTheory.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
+
+export const NumT = () => {
+    return (
+        <div>
+            <h2>NumTheory</h2>
+        </div>
+    )
+}
