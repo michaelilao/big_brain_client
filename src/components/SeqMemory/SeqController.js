64757cbc06ed47caba241b5766736cca84e39c02
justinprez <prezj@mcmaster.ca>
2021-04-12 10:19:23 -0400

    Added rules interface to sequence memory

--- src/components/SeqMemory/SeqController.js
+++ src/components/SeqMemory/SeqController.js
@@ -1,20 +1,36 @@
 import React, { useState,  useEffect } from "react";
-import { Container } from 'react-bootstrap';
+import { Container, Button, Modal } from 'react-bootstrap';
 import { SeqMemory } from './SeqMemory'
 import { SeqMemoryShow } from './SeqMemoryShow'
 import './SeqMemory.scss'
 
 export const SeqController = ({setUserScore, setScreen, nextScreen}) => { 
     const [startGame, setStartGame] = useState(false)
+    const [show, setShow] = useState(true)
     
     const handleStartGame = () => {
         setStartGame(!startGame)
         console.log('here')
     }
 
+    const handleShowSeq = () => {
+        setShow(!show)
+    }
+
     return (
         <Container className="seqController">
-        {!startGame && 
+            <Modal show={show}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Sequence Memory Rules:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body> {'Memorize the order that the block change colour! To earn points click blocks in the same order they appeared!'} </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleShowSeq}>
+                        Start Sequence
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        {!startGame && !show &&
             <SeqMemoryShow setStartGame ={handleStartGame}/>
         }
         {startGame && 
