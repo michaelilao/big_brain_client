dd106e5756ba9f0b670483c6935fe30b4818f331
michael ilao <ilaom@mcmaster.ca>
2021-04-13 19:12:51 -0400

    final build

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
 
   useEffect(() => {
     setUserScore([])
+    setShowAnalytics(false)
   }, [])
   const handleSetScreen = (screen) => {
     setScreen(screen)
--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -7,60 +7,78 @@ import './Analytics.scss'
 
 
 export const Analytics = ({ userScores, metrics }) => {
-    bellcurve(Highcharts);
+    (bellcurve)(Highcharts)
+
 
     useEffect(() => {
     }, [metrics])
-    const numScores = metrics.numUsers
-    const generatePoint = (x, index) => {
-        const mean = metrics.avgMiniGameScores[index]
-        const stdDev = metrics.stdDeviation[index]
-        const factor = 1 / (stdDev * Math.sqrt(2 * Math.PI))
-        const ePower = -1 / 2 * Math.pow(((x - mean) / stdDev), 2)
-        return factor * Math.exp(ePower)
-    }
-    const generateDataSet = (index) => {
-        const miniGameDataSet = []
-        for (var i = 0; i < numScores; i++) {
-            miniGameDataSet.push(generatePoint(i, index))
+    const generateCategories = () => {
+        const categories = []
+        var current = 0
+        for (var i = 0; i <10; i++)
+        {
+            if(current === 0) {
+                categories.push(`${current}-${current+10}`)
+                current = current+11
+            }
+            else {
+                categories.push(`${current}-${current+9}`)
+                current = current+10
+            }
         }
-        return miniGameDataSet
+        return categories
     }
-    const getPercentile = (value, mean, stdDev) => {
-        const z = Math.abs(value-mean)/stdDev
-        if (z < -6.5)
-            return 0.0;
-        if (z > 6.5)
-            return 1.0;
-        var factK = 1;
-        var sum = 0;
-        var term = 1;
-        var k = 0;
-        var loopStop = Math.exp(-23);
-        while (Math.abs(term) > loopStop) {
-            term = 0.3989422804 * Math.pow(-1, k) * Math.pow(z, k) / (2 * k + 1) / Math.pow(2, k) * Math.pow(z, k + 1) / factK;
-            sum += term;
-            k++;
-            factK *= k;
-        }
-        sum += 0.5;
-        return sum;
+    const generateColour = (index, score) => {
+        if (index === Math.floor(score/10)) return '#1F75FE'
+        else return '#B9DFFF'
+        
+    } 
+    const generateColumns = (index, score) => {
+        return metrics.percentileMiniGameScores[index].map((value, index) => {
+            return {
+                x: index,
+                y: value,
+                color: generateColour(index, score),
+            }
+        })
     }
-
-    
-
+    const getMaxPercentile = (index) => {
+        return Math.max(...metrics.percentileMiniGameScores[index]) + 1
+    }
     const generateMiniGameCharts = () => {
         return userScores.map((score, index) => {
             const options = {
+                chart: {
+                    type: 'column'
+                },
                 title: {
                     text: `MiniGame ${index+1}`
                 },
-                baseSeries: 1,
-            
+                xAxis: {
+                    categories: generateCategories(),
+                    crosshair: true,
+                    title: {
+                        text: 'MiniGame Scores (%)'
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    max: getMaxPercentile(index),
+                    title: {
+                        text: 'Number of Users'
+                    }
+                },
                 series: [
                     {
-                    type: 'scatter',
-                    data: metrics.miniGameScores[index]
+                    type: 'column',
+                    name: 'Playerbase Scores',
+                    color: '#B9DFFF',
+                    data: generateColumns(index,score)
+                    },
+                    {
+                        type: 'column',
+                        name: 'User Score',
+                        color: '#1F75FE',
                     }
                 ]
             }
@@ -76,6 +94,12 @@ export const Analytics = ({ userScores, metrics }) => {
     }
     return (
         <Container id="Analytics" fluid className="px-5">
+            <Row className="pb-5">
+                <Col style={{'font-size': '2rem'}} className="text-center">
+                <p>The <span style={{color: '#B9DFFF'}}>lighter blue</span> indicates the player base score</p>
+                <p>The <span style={{color: '#1F75FE'}}>darker blue</span> indicates where your score falls</p>
+                </Col>
+            </Row>
             {metrics && (
                 <Row>
                     {generateMiniGameCharts()}
